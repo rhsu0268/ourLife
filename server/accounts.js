b53ec94d6768f6c496eb0067201b0b9b85372654
Richard Hsu <rhsu@gwu.edu>
2015-10-19 18:51:10 -0400

    Tested functional verification email and password reset email

--- server/accounts.js
+++ server/accounts.js
@@ -9,7 +9,7 @@ Accounts.emailTemplates.verifyEmail.subject = function(user) {
   return 'Welcome to ourLife' ;
 },
 Accounts.emailTemplates.verifyEmail.text = function(user, url) {
-  return 'Click on the link below to verify your address: ' + url;
+  return 'Hi,' + '\n\n' + 'Click on the link below to verify your address: ' + '\n\n' + url + '\n\n' + '-The OurLife Team';
 }
 
 Accounts.validateLoginAttempt(function(attemptInfo) {
@@ -29,3 +29,39 @@ Accounts.validateLoginAttempt(function(attemptInfo) {
 
   return true;
 });
+
+Accounts.emailTemplates.resetPassword.subject = function(user) {
+  return 'Reset Your Life' ;
+
+},
+Accounts.emailTemplates.resetPassword.text = function(user, url) {
+
+  return 'Hi,' + '\n\n' + 'Click on the link below to reset your password: ' + '\n\n' + url + '\n\n' + '-The OurLife Team';
+}
+
+/*
+Accounts.sendResetPasswordEmail = function (userId, email) {
+  // Make sure the user exists, and email is one of their addresses.
+  var user = Meteor.users.findOne(userId);
+  if (!user)
+    throw new Error("Can't find user");
+  // pick the first email if we weren't passed an email.
+  if (!email && user.emails && user.emails[0])
+    email = user.emails[0].address;
+  // make sure we have a valid email
+  if (!email || !_.contains(_.pluck(user.emails || [], 'address'), email))
+    throw new Error("No such email for user.");
+
+  var token = Random.id();
+  var when = new Date();
+  Meteor.users.update(userId, {$set: {
+    "services.password.reset": {
+      token: token,
+      email: email,
+      when: when
+    }
+  }});
+}
+*/
+
+
